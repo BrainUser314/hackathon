28746a9744588653be84e3d40c8323c7f4bc594f
tech <tech@d-2-elch-rf.rf.local>
2024-06-05 12:32:13 +0300

    intro files added

--- IntroScreen.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-func _physics_process(delta):
-	if Input.is_action_just_pressed("exit"):
-		get_tree().quit()
-		
-		
-func _on_start_btn_pressed():
-	get_tree().change_scene_to_file("res://levels/frontlawn/frontlawn.tscn") # Replace with function body.
